41638863bfdb6301b955e27d9635e90ea432e36d
milanOffermans <Milan.offermans@gmail.com>
2025-04-05 19:28:59 +0200

    init from vs code

--- FeedBuf/FeedBuf/obj/Release/net8.0-windows/FeedBuf.AssemblyInfo.cs
+++ FeedBuf/FeedBuf/obj/Release/net8.0-windows/FeedBuf.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FeedBuf")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf897e7f4b08b684b1de79de70ba7338d7cde7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FeedBuf")]
