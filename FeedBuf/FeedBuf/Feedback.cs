f444fbf192adc6681db67fe0ec500d775ad88873
Jurian reuleaux <2406647reuleaux@zuyd.nl>
2025-04-06 16:16:03 +0200

    Feedback class setup

---
+++ FeedBuf/FeedBuf/Feedback.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedBuf
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Author ZuydUser { get; set; }
+        public Student Student { get; set; }
+        public Goal Goal { get; set; }
+        public Actions List<Actions> { get; set; }
+
+        public Feedback(int id, string text, Author zuydUser, Student student, Goal goal, Actions List<Actions>)
+        {
+            Id = id;
+            Text = text;
+            ZuydUser = zuydUser;
+            Student = student;
+            Goal = goal;
+            Actions = List<Actions>;
+        }
+    }
+}
